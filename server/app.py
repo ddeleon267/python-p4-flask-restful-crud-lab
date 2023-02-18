4ea95e730c184c4bc9cd0fedcd239ace6f0be42f
Dominique DeLeon <dominique.a.deleon@gmail.com>
2023-02-17 22:03:53 -0500

    Done

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
 from models import db, Plant
+import pdb
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
@@ -42,8 +43,33 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
+        # pdb.set_trace()
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        # pdb.set_trace()
+        db.session.add(plant)
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+    
+        return response
 
 api.add_resource(PlantByID, '/plants/<int:id>')
         
